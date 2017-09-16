1b238e35397e26a12d2874422e471bf86b01a376
SeanDu <aijisud@gmail.com>
2017-09-16 15:09:18 +0800

    tickmonitor

---
+++ tickmonitor.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import tushare as ts
+import numpy as np
+from mail import mail
+import time
+
+
+today = time.strftime("%Y-%m-%d")
+
+stock_code = '002312'
+tick_volume = 1888
+tick_type = '买盘'
+
+#today = "2017-09-15"
+
+sub = "*ST三泰[002312]1888买单"
+
+
+df = ts.get_tick_data(stock_code, today)
+
+if len(df) <= 3:
+    """
+                   time  price  change  volume  amount  type
+    0  alert("当天没有数据");    NaN     NaN     NaN     NaN   NaN
+    1   window.close();    NaN     NaN     NaN     NaN   NaN
+    2         </script>    NaN     NaN     NaN     NaN   NaN
+    """
+    exit(0)
+
+selected = df[(df.volume == tick_volume) & (df.type == tick_type)]
+if len(selected) == 0:
+    exit(0)
+
+mail.send(sub, str(selected))
+
+if len(selected) >= 3:
+    time.sleep(5)
+    mail.send(sub, str(selected))
+    time.sleep(5)
+    mail.send(sub, str(selected))
+
+#end
