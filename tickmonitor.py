8a544af6e6bb414db944028ac77f0ecec4bda607
SeanDu <aijisud@gmail.com>
2017-09-17 21:55:19 +0800

    change api, get_today_ticks

--- tickmonitor.py
+++ tickmonitor.py
@@ -17,36 +17,45 @@ tick_type = '买盘'
 sub = "*ST三泰[002312]1888买单"
 
 
-df = ts.get_tick_data(stock_code, today)
+print('start')
 
-print(time.strftime("%Y-%m-%d %H:%M:%S"))
+try:
+    print('get_today_ticks')
+    df = ts.get_today_ticks(stock_code)
 
-if len(df) <= 3:
-    """
-                   time  price  change  volume  amount  type
-    0  alert("当天没有数据");    NaN     NaN     NaN     NaN   NaN
-    1   window.close();    NaN     NaN     NaN     NaN   NaN
-    2         </script>    NaN     NaN     NaN     NaN   NaN
-    """
-    print("no data that day")
-    exit(0)
+    print('get ok')
+    print(time.strftime("%Y-%m-%d %H:%M:%S"))
 
-selected = df[(df.volume == tick_volume) & (df.type == tick_type)]
-if len(selected) == 0:
-    print("no data selected")
-    exit(0)
+    if len(df) <= 3:
+        """
+                       time  price  change  volume  amount  type
+        0  alert("当天没有数据");    NaN     NaN     NaN     NaN   NaN
+        1   window.close();    NaN     NaN     NaN     NaN   NaN
+        2         </script>    NaN     NaN     NaN     NaN   NaN
+        """
+        print("no data today")
+        exit(0)
 
+    selected = df[(df.volume == tick_volume) & (df.type == tick_type)]
+    if len(selected) == 0:
+        print("no data selected")
+        exit(0)
 
-print("bingo")
-mail.send(sub, str(selected))
-
-if len(selected) >= 3:
-    print(">= 3")
-    time.sleep(5)
-    mail.send(sub, str(selected))
-    time.sleep(5)
+    print("bingo")
     mail.send(sub, str(selected))
 
-print("the end")
+    if len(selected) >= 3:
+        print(">= 3")
+        time.sleep(5)
+        mail.send(sub, str(selected))
+        time.sleep(5)
+        mail.send(sub, str(selected))
+
+    print("the end")
+except Exception as e:
+    print("Exception:" + str(e))
+    print("the end")
+    exit(0)
+
 
 #end
